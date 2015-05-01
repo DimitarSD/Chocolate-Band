ee13816918689ab8a3489ed5581d2c1e97bed438
DimitarSD <dsd363978@yahoo.com>
2015-05-01 14:44:46 +0300

    Add chocolate-band solution

---
+++ Calculate/Calculate/script/calculator.js
@@ -0,0 +1,19 @@
+﻿var calculateButton = document.getElementById('calculate');
+
+calculateButton.onclick = function () {
+    var chocolateNumber = document.getElementById('chocolate').value;
+    var milisecond = document.getElementById('miliseconds').value;
+    var temperature = document.getElementById('temperature').value;
+
+    var result = parseInt(chocolateNumber) * 3 + parseInt(milisecond) - 10 / parseInt(temperature);
+
+    var divElement = document.getElementById('output');
+    divElement.innerText = result + ' U';
+
+};
+
+var backButton = document.getElementById('back');
+
+backButton.onclick = function () {
+    parent.location.href = 'index.html';
+};
---
+++ Calculate/Calculate/script/canvas.js
@@ -0,0 +1,23 @@
+﻿var canvas = document.getElementById('the-canvas');
+var ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.fillStyle = "black";
+ctx.arc(105, 70, 40, 0, 2 * Math.PI);
+ctx.fillStyle = 'black';
+ctx.fill();
+ctx.moveTo(50, 45);
+ctx.lineTo(70, 50);
+ctx.lineTo(60, 35);
+ctx.moveTo(65, 27);
+ctx.lineTo(70, 50);
+ctx.lineTo(78, 30);
+ctx.fillStyle = 'black';
+ctx.fill();
+ctx.closePath();
+ctx.fillStyle = '#4cff00';
+ctx.fillRect(80, 50, 50, 10);
+ctx.fillStyle = 'black';
+ctx.fillRect(90, 53, 10, 6);
+ctx.fillRect(115, 53, 10, 6);
+ctx.stroke();
